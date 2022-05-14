f18c14bd8c123d2ccbd32bd7e8dd59652e015106
AlessanderLCarazas <104175707+AlessanderLCarazas@users.noreply.github.com>
2022-05-13 22:25:49 -0500

    Add files via upload

---
+++ LAB06_GRUPO_A_20190755_ EJECUTABLE_ALESSANDER_CARAZAS/Ejercicio 2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Alumno.h"
+
+using namespace std;
+
+int main()
+{
+    Alumno a1=Alumno("Alessander Jesus Carazas Quispe",20190755,12,13,12);
+	a1.prom_not();
+    return 0;
+}
